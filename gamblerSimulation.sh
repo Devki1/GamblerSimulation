cbb30e4571e302f8b435cfe190667d92b7ee5cf0
Devki1 <gdev3195@gmail.com>
2020-02-27 17:34:36 +0530

    ADDED:Calculate total win and loss

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -8,20 +8,30 @@ LOSS=0
 stake=100
 half_stake=$(($stake/2))
 full_stake=$(($half_stake+$stake))
-while ((half_stake < stake && full_stake > stake)) 
-do 
-	checkGambler=$((RANDOM%2))
-	case $checkGambler in
-		0)
-			stake=$((stake+BET))
-			((WIN++))
-		;;
-		1)
-			stake=$((stake-BET))
-			((LOSS++))
-		;;
-	esac
+for((day=1;day<=20;day++))
+do
+	stake=100
+	half_stake=$(($stake/2))
+	full_stake=$(($half_stake+$stake))
+	while ((half_stake < stake && full_stake > stake)) 
+	do 
+		checkGambler=$((RANDOM%2))
+		case $checkGambler in
+			0)
+				stake=$((stake+BET))
+				((WIN++))
+			;;
+			1)
+				stake=$((stake-BET))
+				((LOSS++))
+			;;
+		esac
+done	
+array[$day]=WIN:$WIN,LOSS:$LOSS	
 done
-echo "you resigned the day"
-echo "Present stake"=$stake
+for value in ${array[@]}
+do
+	echo $value
+done
+
 
