4f3c4344863dfc361ae377b14048891872cfb0c8
Devki1 <gdev3195@gmail.com>
2020-02-27 14:13:34 +0530

    ADDED:Check stake win oor loss

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -1,6 +1,18 @@
 #!/bin/bash -x
 echo "Start bet with stake 100"
 #Constant
-Bet=1
+BET=1
+WIN=0
+LOSS=1
 #Variables
 stake=100
+#Generate two random value
+checkGambler=$((RANDOM%2))
+if((checkGambler == WIN))
+then
+	echo "You stake  is win"
+	stake=$((stake+BET))
+else
+	echo "You loss stake"
+	stake=$((stake-BET))
+fi
