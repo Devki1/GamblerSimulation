780b25ffc3783021a8d46f7c5b1aeb6bf44ec8de
Devki1 <gdev3195@gmail.com>
2020-02-28 09:38:35 +0530

    ADDED:Check the total number of day win and lost in month

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -5,15 +5,19 @@ BET=1
 WIN=0
 LOSS=0
 #Variables
+count=0
 stake=100
-half_stake=$(($stake/2))
-full_stake=$(($half_stake+$stake))
+wincount=0
+losscount=0
+tempStake=$stake
+tempStake1=$stake
 for((day=1;day<=20;day++))
 do
 	stake=100
 	half_stake=$(($stake/2))
 	full_stake=$(($half_stake+$stake))
-	while ((half_stake < stake && full_stake > stake)) 
+	loss_stake=$(($stake-$half_stake))
+	while (($stake!=$full_stake&&$stake!= $loss_stake)) 
 	do 
 		checkGambler=$((RANDOM%2))
 		case $checkGambler in
@@ -26,12 +30,27 @@ do
 				((LOSS++))
 			;;
 		esac
-done	
-array[$day]=WIN:$WIN,LOSS:$LOSS	
+	done	
+case $stake in
+	$full_stake)
+			echo "you won for the day"
+			echo "Total win amount:" $half_stake
+			tempStake=$((tempStake+half_stake))
+			stakeWinArray[$((count++))]=$tempStake
+			((wincount++))
+			;;
+	$loss_stake)
+			echo "you loss"
+			echo "Total loss amount:" $half_stake
+			tempStake1=$((tempStake1-half_stake))
+			stakeLossArray[$((count++))]=$tempStake1
+			((losscount++))
+			;;
+esac
 done
-for value in ${array[@]}
-do
-	echo $value
-done
-
-
+echo ${stakeWinArray[@]}
+echo ${stakeLossArray[@]}
+echo "no of day loss" $losscount
+echo "you lost by:" $((losscount*half_stake))
+echo "no of day win:" $wincount
+echo "you win by:"$((wincount*half_stake))
